4f5443b4af24a31dcc66707d3d60d88400fe3cb6
hosni <hosni.hassen5876@gmail.com>
2019-10-17 17:25:54 +0100

    I have made a triangle

--- triangle.pas
+++ triangle.pas
@@ -1,6 +1,15 @@
 program triangle;
 uses crt;
+var
+  n,i:integer;
+  ch:string;
 begin
-  
+  readln(n);
+  ch:='';
+  for i:=1 to n do
+  begin
+    ch:=ch+'*';
+    WriteLn(ch);
+  end;
   readln;
 end.
